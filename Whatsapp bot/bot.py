7bad710d1e3be873269ee3b58153b3b9c8e077b0
Rutvik Deshpande <rutvikde2001@gmail.com>
2022-12-17 02:05:06 +0530

    Whatsapp bot 2.0

--- Whatsapp bot/bot.py
+++ Whatsapp bot/bot.py
@@ -4,7 +4,7 @@ import os
 from bs4 import BeautifulSoup
 import http.cookiejar ## http.cookiejar in python3
 from whatsapp import WhatsApp
-
+import json
 
 nrows = 0
 mypass = os.getcwd()+"/password.txt"
@@ -29,10 +29,12 @@ def getdetails():
     soup = BeautifulSoup(x,features="html5lib")
     return soup
 
+with open("data.txt", 'r') as f:
+    try:
+        data = json.load(f)
+    except:
+        data = {}
 
-s = getdetails()
-for table in s.findAll('table',{"class":"table"}):
-        nrows = len(table.findAll(lambda tag: tag.name == 'tr' and tag.findParent('table') == table))
 
 
 whatsapp = WhatsApp(50,session=r'C:\Users\{}\AppData\Local\Google\Chrome\User Data'.format(os.getlogin())) #Device user name 
@@ -41,22 +43,26 @@ while True:
     try:
         soup = getdetails()
         for table in soup.findAll('table',{"class":"table"}):
-            number_of_rows = len(table.findAll(lambda tag: tag.name == 'tr' and tag.findParent('table') == table))
-            top = table.findAll('td',{"class":"mailbox-subject"},limit=1)
-            t = top[0].find("a")
-        if number_of_rows > nrows:
-            nrows = number_of_rows      
-            whatsapp.send_message("To cut the veins","*Placement Update*\n\n*Subject:* "+t.text) 
-            whatsapp.send_message("Blah blah blah blah","*Placement Update*\n\n*Subject:* "+t.text)
-            whatsapp.send_message("SAKEC Family","*Placement Update*\n\n*Subject:* "+t.text)
-            whatsapp.send_message("Send notes","*Placement Update*\n\n*Subject:* "+t.text)
-            whatsapp.send_message("Aditya Shah Sakec","*Placement Update*\n\n*Subject:* "+t.text)
-            whatsapp.send_message("Jagjot Singh","*Placement Update*\n\n*Subject:* "+t.text)
-            print(whatsapp.send_message("Unofficial BE Project","*Placement Update*\n\n*Subject:* "+t.text))  
+            #number_of_rows = len(table.findAll(lambda tag: tag.name == 'tr' and tag.findParent('table') == table))
+            top = table.findAll('td',{"class":"mailbox-subject"})
+            for i in top:
+                t = i.find("a")
+                if(data.get(t.text) is None):
+                    data[str(t.text)]=1
+                    whatsapp.send_message("To cut the veins","*Placement Update*\n\n*Subject:* "+t.text) 
+                    whatsapp.send_message("Blah blah blah blah","*Placement Update*\n\n*Subject:* "+t.text)
+                    whatsapp.send_message("SAKEC Family","*Placement Update*\n\n*Subject:* "+t.text)
+                    whatsapp.send_message("Send notes","*Placement Update*\n\n*Subject:* "+t.text)
+                    whatsapp.send_message("Abhay Gori Sakec","*Placement Update*\n\n*Subject:* "+t.text)
+                    whatsapp.send_message("Aditya Shah Sakec","*Placement Update*\n\n*Subject:* "+t.text)
+                    whatsapp.send_message("Jagjot Singh","*Placement Update*\n\n*Subject:* "+t.text)
+                    print(whatsapp.send_message("Unofficial BE Project","*Placement Update*\n\n*Subject:* "+t.text))
+                    with open("data.txt", 'w') as f:
+                        jsonserial = json.dumps(data, indent=4)
+                        f.write(jsonserial)
     except:
         print("Error in connecting")
-        
-    # short delay between notifications
-    time.sleep(900)
     
+    # short delay between notifications
+    time.sleep(5)
     
